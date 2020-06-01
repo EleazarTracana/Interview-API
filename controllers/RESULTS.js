bcfb914393475d601a93e7655e4aa829f04b479c
eleazartracana <eleazartracana@gmail.com>
2020-06-01 20:02:45 -0300

    se deja base para mat en results

--- controllers/RESULTS.js
+++ controllers/RESULTS.js
@@ -1,8 +1,11 @@
 var client = require('../base_de_datos/Cliente')
 var candidate_functions = require('../controllers/CANDIDATES');
 
-async function get_next_question(dni_user,question,pool_id){
-  
+/*faltaria el metodo que actualiza el resultado del usuario y probar y modificar
+el metodo gext_next_question a tu gusto*/
+
+async function get_next_question(dni_user,pool_id){  
+
     var candidate_db = await client.candidates(),
         pool_db   = await client.tecnologies(),
         candidate = await candidate_db.findOne({dni:dni_user}),
@@ -13,9 +16,35 @@ async function get_next_question(dni_user,question,pool_id){
         current_length_candidate = candidate_questions.length;
 
         candidate_questions.forEach(question => {current_sum_candidate += question.score})
-        let next_difficulty = Math.round(current_sum_candidate/current_length_candidate); 
-        
-        for(var question in pool_question)
+        var current_value = Math.round(current_sum_candidate/current_length_candidate),
+            filtered_questions = pool_questions.filter(e => !candidate_questions.includes(e)),
+            next_difficulty = GetNextDifficulty(current_value);
 
+        var next_question = filtered_questions.find(e => e.difficulty == next_difficulty);
+        if(next_question == null){
+           //implementar logica si no hay mas preguntas con esa dificultad
+        }
 
+        return next_question;
+
+};
+var GetNextDifficulty = (total) =>{
+    var next_value = 1;
+    const highest_score = 5,
+          highest_medium_score = 4,
+          medium_score = 3,
+          medium_slow_score =2,
+          slow_score = 1;
+          
+          //lo hice de ejemplo mat pero a es a tu gusto
+          if(total == highest_score){
+            next_value = highest_score
+          }else if(total == highest_medium_score) {
+            next_value = highest_score
+          }else if(total == medium_score){
+            next_value = medium_score
+          }else if(total == medium_slow_score){
+            next_value  = slow_score;
+          }
+    return next_value;
 }
